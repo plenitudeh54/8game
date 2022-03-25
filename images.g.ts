54df3701917fa2679a48b990c07f45ded8562137
plenitudeh54 <plenitudeh54@gmail.com>
2022-03-25 16:05:04 +0100

    Perso

---
+++ images.g.ts
@@ -0,0 +1,75 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "escobar":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 5 7 7 7 7 7 7 . . . . . 
+. . 7 7 7 7 7 7 7 7 7 . . . . . 
+. . . . f d d d d f f . . . . . 
+. . . . d d d d d d f . . . . . 
+. . . . f f d d d d d . . . . . 
+. . . . d f d d d d d . . . . . 
+. . . . . . . . d d d . . . . . 
+. . . . . . . 7 7 7 7 . . . . . 
+. . . . 7 7 7 7 7 7 7 . . . . . 
+. . . . 7 7 7 7 7 7 7 . . . . . 
+. . . . 7 7 7 7 7 7 7 . . . . . 
+. . . . 8 8 8 8 d d 8 . . . . . 
+. . . . 8 8 8 8 8 8 8 . . . . . 
+. . . . 8 8 8 8 8 8 8 . . . . . 
+. . . f f f f f f f f . . . . . 
+`;
+            case "image2":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 8 8 8 8 8 8 8 5 . . . . 
+. . . . 8 8 8 8 8 8 8 8 8 8 . . 
+. . . . f f f f f f d d . . . . 
+. . . . f d d d d d d f . . . . 
+. . . . d d d d d d d d . . . . 
+. . . . d d d d d d d 3 . . . . 
+. . . . d d d d . . . . . . . . 
+. . . . 8 8 8 8 8 . . . . . . . 
+. . . . 8 8 8 8 8 8 8 . . . . . 
+. . . . 8 8 8 8 8 8 8 . . . . . 
+. . . . 8 8 8 8 8 8 8 . . . . . 
+. . . . d d f f f f f . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+`;
+            case "image3":
+            case "myImage0":return img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
