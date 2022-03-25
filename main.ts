ebed5ce31a48fbc5050fca97d1f28a5ffd0f3834
plenitudeh54 <plenitudeh54@gmail.com>
2022-03-25 14:39:54 +0100

    Plan des niveau

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
-    if (otherSprite == Play && controller.A.isPressed()) {
-        Niveau += 1
-        Controle_de_niveau()
-    }
-})
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
 function Controle_de_niveau () {
     if (Niveau == 0) {
         scene.setBackgroundImage(img`
@@ -315,7 +314,15 @@ function Controle_de_niveau () {
         }
     }
 }
-function Demarrage () {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    if (otherSprite == Play && controller.A.isPressed()) {
+        Niveau += 1
+        Controle_de_niveau()
+    }
+})
+function Game_start () {
+    Niveau = 10
+    Niveau_actuel = 1
     scene.setBackgroundImage(img`
         9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
         9999999999999999999999999999999999999999999999999999111111111119999999999999999999999999999999999999991111999999999999999999999999999999999999999999111111111111
@@ -439,30 +446,62 @@ function Demarrage () {
         bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
         `)
     Acacio_G = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
-        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . . . . . . . . . . . . 
         . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
+        . . . f f e e e e f 2 f . . . . 
+        . . f f e e e e f 2 2 2 f . . . 
+        . . f e e e f f e e e e f . . . 
+        . . f f f f e e 2 2 2 2 e f . . 
+        . . f e 2 2 2 f f f f e 2 f . . 
+        . f f f f f f f e e e f f f . . 
+        . f f e 4 4 e b f 4 4 e e f . . 
+        . f e e 4 d 4 1 f d d e f . . . 
+        . . f e e e e e d d d f . . . . 
+        . . . . f 4 d d e 4 e f . . . . 
+        . . . . f e d d e 2 2 f . . . . 
+        . . . f f f e e f 5 5 f f . . . 
+        . . . f f f f f f f f f f . . . 
+        . . . . f f . . . f f f . . . . 
         `, SpriteKind.Player)
+    Acacio_G.setPosition(1, 90)
     controller.moveSprite(Acacio_G)
     Acacio_G.setStayInScreen(true)
+    info.setLife(6)
 }
+function animeFound () {
+    heroIdGauche = animation.createAnimation(ActionKind.Walking, 1)
+    animation.attachAnimation(Acacio_G, heroIdGauche)
+}
+function Plan_de_niveau () {
+    if (Niveau_actuel == 1) {
+        tiles.setCurrentTilemap(tilemap`niveau1`)
+    } else if (Niveau_actuel == 2) {
+    	
+    } else if (Niveau_actuel == 3) {
+    	
+    } else if (Niveau_actuel == 4) {
+    	
+    } else if (Niveau_actuel == 5) {
+    	
+    } else if (Niveau_actuel == 6) {
+    	
+    } else if (Niveau_actuel == 7) {
+    	
+    } else if (Niveau_actuel == 8) {
+    	
+    } else if (Niveau_actuel == 9) {
+    	
+    } else if (Niveau_actuel == 10) {
+    	
+    } else {
+    	
+    }
+}
+let heroIdGauche: animation.Animation = null
 let Acacio_G: Sprite = null
+let Niveau_actuel = 0
 let Curseur: Sprite = null
 let Aide: Sprite = null
 let Play: Sprite = null
 let Niveau = 0
-Niveau = 0
-Demarrage()
+Game_start()
